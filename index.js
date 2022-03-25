b3a0a572bd48e0f461a4aae87d17d373e200f880
Maham Ali <50532268+mahammi@users.noreply.github.com>
2022-03-25 12:02:01 -0400

    Add search functionality

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-//object storing API data
-let randID = Math.floor((Math.random()*500) + 1);
-const pokemonDiv = document.querySelector('.pokemon-card');
 
+const pokemonDiv = document.querySelector('.pokemon-card');
+const submit = document.querySelector('button');
+
+//object storing API data
 const data = {
     url: 'https://pokeapi.co/api/v2/',
     type:'pokemon',
-    id: randID,
+    name: 'pikachu',
 };
 
-const apiURL = `${data.url}${data.type}/${data.id}`;
+//default data = pikachu
+let apiURL = `${data.url}${data.type}/${data.name}`;
 
+//fetch data
 fetch(apiURL)
     .then((data)=>data.json())
-    .then((data)=>displayInfo(data)
-    ).catch(()=>errorMessage());
+    .then((data)=>displayInfo(data))
+    .catch(()=>errorMessage());
+
 
 const displayInfo = (data) => {
-  
-    console.log(data);
     const html =`
         <div class="pokemon-name"><b>${data.name}</b></div>
         <img class="pokemon-img"src=${data.sprites.front_default}>
@@ -31,7 +33,29 @@ const displayInfo = (data) => {
     pokemonDiv.innerHTML = html;
 }
 
+
 const errorMessage = () => {
     console.log("No data");
-    pokemonDiv.innerHTML = `<h1 class="error-message">The Pokemon could not be found. Please try again!</h1>`;
+    pokemonDiv.innerHTML = `<h1 class="error-message">The Pokémon "${data.name}" could not be found. Please try again!</h1>`;
 }
+
+const getPokemon = () => {
+    const pokeName = document.querySelector('input').value.toLowerCase();
+    data.name = pokeName;
+    apiURL = `${data.url}${data.type}/${data.name}`;
+
+    fetch(apiURL)
+        .then((data)=>data.json())
+        .then((data)=>displayInfo(data))
+        .catch(()=>errorMessage());
+}
+
+submit.addEventListener("click", getPokemon);
+
+
+
+
+
+
+
+
